993c054d8283aa1c10c42a0465db82d8f34fec91
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-24 21:22:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,28 @@
 class Song
 
 
-attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist, :genre
 
-@@count = 0
-@@artists = []
-@@genres = []
+  @@count = 0
+  @@artists = []
+  @@genres = []
 
-def initialize(name, artist, genre)
-  @name = name
-  @artist =  artist
-  @genre = genre
-  @@count += 1
-  @@artists << artist
-  @@genres << genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist =  artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+
+  end
+  def self.count
+    @@genres.count
+end
 
 end
-def self.count
-  @@genres.count
-end
+
 
 
 
