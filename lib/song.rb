4e44b14e5a08c2ba031c610d3fa6cac201956d13
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-24 21:14:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,7 @@ attr_accessor :name, :artist, :genre
 
 @@count = 0
 @@artists = []
+@@genres = []
 
 def initialize(name, artist, genre)
   @name = name
@@ -12,6 +13,7 @@ def initialize(name, artist, genre)
   @genre = genre
   @@count += 1
   @@artists << artist
+  @@genres << genre
 
 end
 
